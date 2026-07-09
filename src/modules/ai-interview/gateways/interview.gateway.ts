7904b8d5a8e9e7ff08b7626efbb10ac6b9eaae81
Ritik <160255700+gritik418@users.noreply.github.com>
2026-07-09 13:37:20 +0530

    feat: add middleware in interview gateway

--- src/modules/ai-interview/gateways/interview.gateway.ts
+++ src/modules/ai-interview/gateways/interview.gateway.ts
@@ -3,6 +3,7 @@ import {
   MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -14,6 +15,10 @@ import { WSAuthGuard } from 'src/common/guards/auth/ws-auth.guard';
 import { ZodValidationPipe } from 'src/common/pipes/zod-validation/zod-validation.pipe';
 import { JOIN_INTERVIEW } from '../constants/interview.events';
 import { JoinInterviewSchema } from '../schemas/join-interview.schema';
+import { NextFunction } from 'express';
+import * as cookie from 'cookie';
+import { AUTH_COOKIE_NAME } from 'src/common/constants/cookie-names';
+import { JwtService } from '@nestjs/jwt';
 
 @WebSocketGateway({
   namespace: '/',
@@ -22,10 +27,31 @@ import { JoinInterviewSchema } from '../schemas/join-interview.schema';
   },
 })
 export class InterviewGateway
-  implements OnGatewayConnection, OnGatewayDisconnect
+  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
   @WebSocketServer()
   server: Server;
+  constructor(private readonly jwtService: JwtService) {}
+
+  afterInit(server: Server) {
+    server.use(async (socket: Socket, next: NextFunction) => {
+      try {
+        const cookies = cookie.parse(socket.handshake.headers.cookie ?? '');
+
+        const token = cookies[AUTH_COOKIE_NAME];
+
+        if (!token) return next(new Error('Unauthorized.'));
+
+        const payload = await this.jwtService.verifyAsync(token);
+
+        socket.user = payload;
+
+        next();
+      } catch (error) {
+        next(new Error('Unauthorized.'));
+      }
+    });
+  }
 
   handleConnection(client: Socket) {
     console.log('Connected', client.user);
